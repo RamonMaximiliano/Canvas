cef1d5e22e72fcb80f0c6e62bf980b9d7710fbf1
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-01-04 00:48:11 -0300

    add color

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function cleanBoard(){
     window.location.reload()
 }
+
+document.getElementById("canvas").addEventListener("mousedown", function mouseDown(){
+    console.log("Mouse down")
+})
+
+document.getElementById("canvas").addEventListener("mouseup", function mouseDown(){
+    console.log("mouseup")
+})
+document.getElementById("canvas").addEventListener("mousemove", function mouseDown(){
+    console.log("mousemove")
+})
+
+
+document.querySelector(".colorind").addEventListener("click", function colorChange(){
+            
+
+
+})
